19f6f38bff778dbc468641f87a6a53ccdf3d307e
Morty <zaharzvagin254@gmail.com>
2026-03-26 00:26:33 +0300

    Update ModConfig.java

--- src/main/java/com/seasonaldaycycle/ModConfig.java
+++ src/main/java/com/seasonaldaycycle/ModConfig.java
@@ -7,60 +7,56 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 public class ModConfig {
 
-    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    // --- Spring ---
     public static ForgeConfigSpec.IntValue SPRING_DAY_TICKS;
     public static ForgeConfigSpec.IntValue SPRING_NIGHT_TICKS;
-
-    // --- Summer ---
     public static ForgeConfigSpec.IntValue SUMMER_DAY_TICKS;
     public static ForgeConfigSpec.IntValue SUMMER_NIGHT_TICKS;
-
-    // --- Autumn ---
     public static ForgeConfigSpec.IntValue AUTUMN_DAY_TICKS;
     public static ForgeConfigSpec.IntValue AUTUMN_NIGHT_TICKS;
-
-    // --- Winter ---
     public static ForgeConfigSpec.IntValue WINTER_DAY_TICKS;
     public static ForgeConfigSpec.IntValue WINTER_NIGHT_TICKS;
 
     static {
-        BUILDER.comment("SeasonalDayCycle Configuration",
-            "Values represent how many REAL ticks (at 20 TPS) the day or night lasts.",
-            "Default: 60-minute full cycle (36000 day + 36000 night = 72000 total).",
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.comment("SeasonalDayCycle Configuration",
+            "Values = real ticks (20 TPS) for day or night.",
             "Summer: longer day. Winter: longer night.");
 
-        BUILDER.push("spring");
-        SPRING_DAY_TICKS   = BUILDER.comment("Real ticks for spring daytime (default 36000 = 30min)")
+        builder.push("spring");
+        SPRING_DAY_TICKS   = builder.comment("Real ticks for spring daytime (36000 = 30min)")
             .defineInRange("day_ticks", 36000, 1000, 500000);
-        SPRING_NIGHT_TICKS = BUILDER.comment("Real ticks for spring nighttime (default 36000 = 30min)")
+        SPRING_NIGHT_TICKS = builder.comment("Real ticks for spring nighttime (36000 = 30min)")
             .defineInRange("night_ticks", 36000, 1000, 500000);
-        BUILDER.pop();
+        builder.pop();
 
-        BUILDER.push("summer");
-        SUMMER_DAY_TICKS   = BUILDER.comment("Real ticks for summer daytime (default 42000 = 35min)")
+        builder.push("summer");
+        SUMMER_DAY_TICKS   = builder.comment("Real ticks for summer daytime (42000 = 35min)")
             .defineInRange("day_ticks", 42000, 1000, 500000);
-        SUMMER_NIGHT_TICKS = BUILDER.comment("Real ticks for summer nighttime (default 30000 = 25min)")
+        SUMMER_NIGHT_TICKS = builder.comment("Real ticks for summer nighttime (30000 = 25min)")
             .defineInRange("night_ticks", 30000, 1000, 500000);
-        BUILDER.pop();
+        builder.pop();
 
-        BUILDER.push("autumn");
-        AUTUMN_DAY_TICKS   = BUILDER.comment("Real ticks for autumn daytime (default 36000 = 30min)")
+        builder.push("autumn");
+        AUTUMN_DAY_TICKS   = builder.comment("Real ticks for autumn daytime (36000 = 30min)")
             .defineInRange("day_ticks", 36000, 1000, 500000);
-        AUTUMN_NIGHT_TICKS = BUILDER.comment("Real ticks for autumn nighttime (default 36000 = 30min)")
+        AUTUMN_NIGHT_TICKS = builder.comment("Real ticks for autumn nighttime (36000 = 30min)")
             .defineInRange("night_ticks", 36000, 1000, 500000);
-        BUILDER.pop();
+        builder.pop();
 
-        BUILDER.push("winter");
-        WINTER_DAY_TICKS   = BUILDER.comment("Real ticks for winter daytime (default 30000 = 25min)")
+        builder.push("winter");
+        WINTER_DAY_TICKS   = builder.comment("Real ticks for winter daytime (30000 = 25min)")
             .defineInRange("day_ticks", 30000, 1000, 500000);
-        WINTER_NIGHT_TICKS = BUILDER.comment("Real ticks for winter nighttime (default 42000 = 35min)")
+        WINTER_NIGHT_TICKS = builder.comment("Real ticks for winter nighttime (42000 = 35min)")
             .defineInRange("night_ticks", 42000, 1000, 500000);
-        BUILDER.pop();
+        builder.pop();
 
-        SPEC = BUILDER.build();
+        SPEC = builder.build();
+    }
+
+    public static void register() {
         ModLoadingContext.get().registerConfig(Type.SERVER, SPEC, "seasonaldaycycle-server.toml");
     }
 
