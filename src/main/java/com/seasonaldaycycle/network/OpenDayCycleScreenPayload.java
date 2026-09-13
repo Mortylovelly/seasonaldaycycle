53462320331a8231233fc5ad6a6d07342f956b1c
Morty <zaharzvagin254@gmail.com>
2026-09-13 21:42:59 +0300

    Adapt screen packet to Fabric 1.20.1 networking API

--- src/main/java/com/seasonaldaycycle/network/OpenDayCycleScreenPayload.java
+++ src/main/java/com/seasonaldaycycle/network/OpenDayCycleScreenPayload.java
@@ -1,20 +1,28 @@
 package com.seasonaldaycycle.network;
 
 import com.seasonaldaycycle.SeasonalDayCycle;
-import net.minecraft.network.RegistryByteBuf;
-import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.packet.CustomPayload;
+import net.fabricmc.fabric.api.networking.v1.FabricPacket;
+import net.fabricmc.fabric.api.networking.v1.PacketType;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
-public record OpenDayCycleScreenPayload() implements CustomPayload {
-    public static final CustomPayload.Id<OpenDayCycleScreenPayload> ID =
-            new CustomPayload.Id<>(Identifier.of(SeasonalDayCycle.MODID, "open_daycycle_screen"));
+public record OpenDayCycleScreenPayload() implements FabricPacket {
+    public static final PacketType<OpenDayCycleScreenPayload> TYPE =
+            PacketType.create(
+                    new Identifier(SeasonalDayCycle.MODID, "open_daycycle_screen"),
+                    OpenDayCycleScreenPayload::new
+            );
 
-    public static final PacketCodec<RegistryByteBuf, OpenDayCycleScreenPayload> CODEC =
-            PacketCodec.unit(new OpenDayCycleScreenPayload());
+    public OpenDayCycleScreenPayload(PacketByteBuf ignored) {
+        this();
+    }
 
     @Override
-    public Id<? extends CustomPayload> getId() {
-        return ID;
+    public void write(PacketByteBuf buf) {
+    }
+
+    @Override
+    public PacketType<?> getType() {
+        return TYPE;
     }
 }
